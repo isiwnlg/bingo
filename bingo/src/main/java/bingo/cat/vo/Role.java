6f0016ee0f5aef179405ce03373dd801a8e56560
isiwnlg <isiwnlg@qq.com>
2014-10-30 16:33:58 +0800

    冲突测试
    两个程序员同时修改一个文件

--- bingo/src/main/java/bingo/cat/vo/Role.java
+++ bingo/src/main/java/bingo/cat/vo/Role.java
@@ -7,7 +7,7 @@ import bingo.cat.ObjectType;
 
 public class Role extends CatVoObject {
    private String label;//=Name ?
-   private String Containment;
+   private String Containment;//程序员1修改
    private String supplier;
    private String quidu;
    private boolean navigatable;
